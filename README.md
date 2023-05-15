ec3f2399cd00e702cfc23dff4f639b7c0f47cb8f
sadeq <sadeq.yaqobi9219@gmail.com>
2023-05-15 21:42:57 +0330

    README.md and demo images were added and optimized codes

---
+++ README.md
@@ -0,0 +1,52 @@
+# Trex Game
+
+![Trex Game Screenshot](images/demo-Trex-01.png)
+
+The Trex Game is a simple web-based game where the player controls a running Trex character, avoiding obstacles and scoring points. It is built using HTML, CSS, and JavaScript.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+To play the Trex Game locally, follow these steps:
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/sadeq-yaqobi/Trex-game.git
+2. Navigate to the project directory:
+
+    ```bash
+    cd Trex-game
+3. Open the index.html file in a web browser.
+
+
+## Usage
+Once you have the game running in your web browser, use the following controls:
+
+- Press the **spaceKey** or the **left click** to make the Trex character jump.
+- Avoid obstacles by jumping over them.
+- Your score will increase as you successfully avoid obstacles.
+- The game ends if the Trex character collides with an obstacle.
+
+![Trex Game Screenshot](images/demo-Trex-02.png)
+
+
+## Contributing
+Contributions to the Trex Game project are welcome. If you would like to contribute, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch: `git checkout -b my-new-feature`.
+3. Make your changes and commit them: `git commit -m 'Add some feature'`.
+4. Push to the branch: `git push origin my-new-feature`.
+5. Open a pull request.
+
+Please ensure that your pull request follows the project's coding conventions and includes relevant documentation or tests.
+
+## License
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
--- js/javaScript.js
+++ js/javaScript.js
@@ -1,37 +1,60 @@
+// Get the DOM elements
 var tRex = document.getElementById("trex");
 var cactus = document.getElementById("cactus");
+
+// Initialize the counter
 var counter = 0;
 
+// Function to handle the jump animation
 function jump() {
-  if (tRex.classList == "jump-animate") {
+  // If the trex is already jumping, do nothing
+  if (tRex.classList.contains("jump-animate")) {
     return;
   }
+
+  // Add the jump animation class to the trex
   tRex.classList.add("jump-animate");
+
+  // Remove the jump animation class after 700 milliseconds
   setTimeout(function () {
     tRex.classList.remove("jump-animate");
   }, 700);
 }
+
+// Check for collision and update the score
 var checkDead = setInterval(function () {
-  let tRexBottom = parseInt(
+  // Get the bottom position of the trex and the left position of the cactus
+  var tRexBottom = parseInt(
     window.getComputedStyle(tRex).getPropertyValue("bottom")
   );
-  let cactusLeft = parseInt(
+  var cactusLeft = parseInt(
     window.getComputedStyle(cactus).getPropertyValue("left")
   );
+
+  // Check for collision
   if (tRexBottom <= 76 && cactusLeft <= 90 && cactusLeft >= 19) {
+    // Stop the cactus animation
     cactus.style.animation = "none";
-    alert("Game Over . Score :  " + Math.floor(counter / 100));
+
+    // Display game over message with the score
+    alert("Game Over. Score: " + Math.floor(counter / 100));
+
+    // Reset the counter and restart the cactus animation
     counter = 0;
     cactus.style.animation = "cactusAnimate 2.5s linear infinite";
   } else {
+    // Increment the counter and update the score
     counter++;
     document.getElementById("score").innerHTML = Math.floor(counter / 100);
   }
 }, 10);
 
+// Function to handle the jump on spacebar key press
 function jumpKeypress(event) {
   if (event.which === 32) {
     jump();
   }
 }
+
+// Add event listener for keydown event to trigger jump on spacebar press
 document.addEventListener("keydown", jumpKeypress);
